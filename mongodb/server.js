498ac725c138328b26b62fd3e1541b8e50544af1
wjddusgnl676@naver.com <wjddusgnl676@naver.com>
2022-04-18 01:15:38 +0900

    Express를 활용한 REST API 만들기

--- mongodb/server.js
+++ mongodb/server.js
@@ -1,9 +1,18 @@
 const express = require("express");
-
 const app = express();
 
-app.get("/", function (req, res) {
-  return res.send("hello world!");
+const users = [];
+
+app.use(express.json());
+
+app.get("/user", function (req, res) {
+  return res.send({ users: users });
+});
+
+app.post("/user", function (req, res) {
+  console.log(res.body);
+  users.push({ name: req.body.name, age: req.body.age });
+  return res.send({ success: true });
 });
 
 app.listen(3000, function () {
